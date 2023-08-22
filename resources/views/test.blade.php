1d64c4dbd6b9d972ed96053cdb3ae552ffe76406
carotte98 <thierrymordant@ymail.com>
2023-08-22 16:02:29 +0200

    Revert "Merge branch 'Michelle' into dev"
    This reverts commit 3bb5685e6027268bfa45d2909e95eb66b575e89e.

--- resources/views/test.blade.php
+++
@@ -1,4 +0,0 @@
-<x-layout>
-    TEST
-    TEST
-</x-layout>
--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,6 @@ use App\Http\Controllers\ServiceController;
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-Route::get('/', function () {
-    return view('test');
-});
 
 Route::get('/', [ServiceController::class, 'index']);
 Route::get('/services/{service}', [ServiceController::class, 'show']);
