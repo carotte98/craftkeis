42a0b0407d8df844e124f8e7df0665c0f7325610
lumi963 <123492008+lumi963@users.noreply.github.com>
2023-08-25 11:15:08 +0200

    [25.08] flash message works

--- resources/views/components/flash-message.blade.php
+++ resources/views/components/flash-message.blade.php
@@ -1,5 +1,6 @@
 @if (session()->has('message'))
-    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 3000)" x-show="show" class="fixed top-0 left-1/2 transform -translate-x-1/2 bg-laravel text-white px-48">
+    <div x-data="{ show: true }" x-init="setTimeout(() => show = false, 9000)" x-show="show" 
+        class="absolute top-0 left-1/2 transform -translate-x-1/2 mt-20 w-96 bg-accent text-white p-4 shadow-md">
         <p>
             {{session('message')}}
         </p>
