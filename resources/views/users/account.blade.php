cdf7c29404c35795546279fef236741988f5cf9d
Timnaka <timhengel@hotmail.com>
2023-08-25 09:43:15 +0200

    [25.08] started account dashboard

--- resources/views/users/account.blade.php
+++ resources/views/users/account.blade.php
@@ -3,7 +3,7 @@
     </a>
     <div>
         <x-card>
-            <div>
+            <x-card> {{-- Profile details --}}
                 <img class="w-48 mr-6 mb-6"
                     src="{{ $user->image_address ? asset('storage/' . $user->image_address) : asset('images/no-image.png') }}"
                     alt="profile-picture" />
@@ -13,51 +13,58 @@
                 <div>Email: {{ $user->email }}</div><br>
                 <div>Bio:</div>
                 <div>{{ $user->bio }}</div><br>
-            </div>
-            <a href="/users/{{ $user->id }}/edit">
-                <i class="fa-solid fa-pencil"></i>Edit
-            </a>
-            <a href="/users/account/orders">
-                <i class="fa-solid fa-gear"></i>Orders
-            </a>
-            {{-- inbox for creators --}}
-            <span class="text-lg">
-                <i class="fa-solid fa-inbox"></i>
-                <a href="/users/account/commissions" class="hover:text-accent">Inbox</a>
-            </span>
-        </x-card>
+                <a href="/users/{{ $user->id }}/edit">
+                    <i class="fa-solid fa-pencil"></i>Edit
+                </a>
+                <a href="/users/account/orders">
+                    <i class="fa-solid fa-gear"></i>Orders
+                </a>
+                {{-- inbox for creators --}}
+                <span class="text-lg">
+                    <i class="fa-solid fa-inbox"></i>
+                    <a href="/users/account/commissions" class="hover:text-accent">Inbox</a>
+                </span>
+            </x-card>
 
-    <div>
-        @foreach ($user->orderClient as $order)
-        <h2>
-            {{$order}}
-            <strong>Title: {{$order->title}}</strong>
-            <strong>Description: {{$order->description}}</strong>
-        </h2>
-        @endforeach
-    </div>
-    <hr>
-    {{-- *TEST --}}
-    {{-- Click to add user to contacts --}}
-    <h2>All Users (click to add to contacts)</h2>
-    <div>
-        @foreach ($tempContacts as $contact)
+            <x-card> {{-- Bank details --}}
+                <h2>
+                    Bank Details
+                </h2>
+            </x-card>
+
             <div>
-                <strong>
-                    <a href="/users/account/chat/{{ $contact->id }}">{{ $contact->name }}</a>
-                </strong>
+                @foreach ($user->orderClient as $order)
+                    <h2>
+                        {{$order}}
+                        <strong>Title: {{$order->title}}</strong>
+                        <strong>Description: {{$order->description}}</strong>
+                    </h2>
+                @endforeach
+                <hr>
+                {{-- *TEST --}}
+                {{-- Click to add user to contacts --}}
+                <h2>All Users (click to add to contacts)</h2>
+                <div>
+                    @foreach ($tempContacts as $contact)
+                        <div>
+                            <strong>
+                                <a href="/users/account/chat/{{ $contact->id }}">{{ $contact->name }}</a>
+                            </strong>
+                        </div>
+                    @endforeach
+                </div>
+                <hr>
+                {{-- Contacts after conversation has been created --}}
+                {{-- Example user clicks to connect with creator --}}
+                <h2>Contacts</h2>
+                @foreach ($contacts as $contact)
+                    <div>
+                        <strong>
+                            <a href="/users/account/chat/conversation/{{ $contact->id }}">{{ $contact->name }}</a>
+                        </strong>
+                    </div>
+                @endforeach
             </div>
-        @endforeach
+        </x-card>
     </div>
-    <hr>
-    {{-- Contacts after conversation has been created --}}
-    {{-- Example user clicks to connect with creator --}}
-    <h2>Contacts</h2>
-    @foreach ($contacts as $contact)
-        <div>
-            <strong>
-                <a href="/users/account/chat/conversation/{{ $contact->id }}">{{ $contact->name }}</a>
-            </strong>
-        </div>
-    @endforeach
 </x-layout>
