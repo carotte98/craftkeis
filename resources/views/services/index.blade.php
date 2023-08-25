51a71528a2673291b6238d2b0aca1db3a9cd4f46
carotte98 <thierrymordant@ymail.com>
2023-08-25 15:31:56 +0200

    Change in gaps on index

--- resources/views/services/index.blade.php
+++ resources/views/services/index.blade.php
@@ -1,6 +1,6 @@
 <x-layout>
 
-    <x-card class="grid grid-cols-2 gap-x-3 w-2/3">
+    <x-card class="grid grid-cols-2 gap-x-6 gap-y-4 w-2/3">
             @if (count($services) == 0)
                 <p>No services found</p>
             @endif
