4fef257f15900c6660b6d40fda3ca43d38d01a6f
Cedric <sousa.cedric91@gmail.com>
2023-08-26 18:02:56 +0200

    [26.08] Fixed error when user has no bank_details & admin layout changes

--- resources/views/components/dashboard-bank.blade.php
+++ resources/views/components/dashboard-bank.blade.php
@@ -2,6 +2,6 @@
     <h2>
         Bank Details
     </h2>
-    <div>Payment Method: {{$user->bank_details->payment_method}}</div>
-    <div>Card Number: {{$user->bank_details->cardNumber}}</div>
+    <div>Payment Method: {{$user->bank_details ? $user->bank_details->payment_method : ""}}</div>
+    <div>Card Number: {{$user->bank_details ? $user->bank_details->payment_method : ""}}</div>
 </x-card-sec>
--- resources/views/users/admin.blade.php
+++ resources/views/users/admin.blade.php
@@ -30,41 +30,41 @@
                     <x-dashboard-orderClient :user=$user />
 
                     <x-dashboard-orderCreator :user=$user /> --}}
-        <x-card-sec class="max-w-screen-xl mx-auto px-5 bg-white min-h-sceen">
-            <div class="flex flex-col items-center">
-                <h2 class="font-bold text-5xl mt-5 tracking-tight">
-                    ADMIN DASHBOARD
-                </h2>
-                <p class="text-neutral-500 text-xl mt-3">
-                    List of all Users
-                </p>
-            </div>
-            <div class="divide-y divide-neutral-200 mx-auto">
-                @foreach ($users as $user)
-                    <div class="py-5">
-                        <details class="group">
-                            <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
-                                <p> {{ $user }}</p>
-                                <span class="transition group-open:rotate-180">
-                                    <svg fill="none" height="24" shape-rendering="geometricPrecision"
-                                        stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
-                                        stroke-width="1.5" viewBox="0 0 24 24" width="24">
-                                        <path d="M6 9l6 6 6-6"></path>
-                                    </svg>
-                                </span>
-                            </summary>
-                            {{-- <x-dashboard-bank :user=$user /> --}}
 
-                            <x-dashboard-service :user=$user />
-
-                            <x-dashboard-orderClient :user=$user />
-
-                            <x-dashboard-orderCreator :user=$user />
-                        </details>
-                    </div>
-                @endforeach
-            </div>
+        <x-card-sec class="flex flex-col items-center">
+            <h2 class="font-bold text-5xl mt-5 tracking-tight">
+                ADMIN DASHBOARD
+            </h2>
+            <p class="text-neutral-500 text-xl mt-3">
+                List of all Users
+            </p>
         </x-card-sec>
+        <div class="divide-y divide-neutral-200 mx-auto">
+            @foreach ($users as $user)
+                <x-card-sec class="py-5">
+                    <details class="group">
+                        <summary class="flex justify-between items-center font-medium cursor-pointer list-none">
+                            <span class="transition group-open:rotate-180">
+                                <svg fill="none" height="24" shape-rendering="geometricPrecision"
+                                    stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                                    stroke-width="1.5" viewBox="0 0 24 24" width="24">
+                                    <path d="M6 9l6 6 6-6"></path>
+                                </svg>
+                            </span>
+                            <p> {{ $user }}</p>
+                        </summary>
+                        <x-dashboard-bank :user=$user />
+
+                        <x-dashboard-service :user=$user />
+
+                        <x-dashboard-orderClient :user=$user />
+
+                        <x-dashboard-orderCreator :user=$user />
+                    </details>
+                </x-card-sec>
+            @endforeach
+
+        </div>
 
         <h2>Contacts</h2>
         @foreach ($contacts as $contact)
